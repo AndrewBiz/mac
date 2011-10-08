b2c9da90e3cb82a58e3daf516fba0f2711b30ca1
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2011-10-09 02:02:46 +0400

    finished FotoEvent class

--- foto_init_event.rb
+++ foto_init_event.rb
@@ -4,7 +4,7 @@ require "yaml"
 require "date"
 require "logger"
 require "mini_exiftool" # gem install mini_exiftool (http://miniexiftool.rubyforge.org/)
-require "progressbar" #gem install progressbar (https://github.com/jfelchner/ruby-progressbar)
+require "progressbar" # gem install progressbar (https://github.com/jfelchner/ruby-progressbar)
 
 # *** GLOBAL Constants and Variables ***
 $DateTimeFormat = '%F %T %z'
@@ -41,18 +41,17 @@ def read_input_params
 
   # read input args
   dir_to_process = ARGV[0]||Dir.pwd
-  if not File.exist?(dir_to_process) 
-    fail("#{dir_to_process} does not exist") #or raise
-  end
-  if not File.directory?(dir_to_process)
-    fail("#{dir_to_process} is not a Directory")
-  end
-
+  fail("#{dir_to_process} does not exist") unless File.exist?(dir_to_process) 
+  fail("#{dir_to_process} is not a Directory") unless File.directory?(dir_to_process)
+  $log.info "Dir to be processed: #{dir_to_process}"
+  
   yaml_name = ARGV[1]||find_first_yaml_file(dir_to_process)
+  fail("- no YAML File found;") unless yaml_name
   # read script_options
   if File.file?(yaml_name)
     begin
       options = YAML.load_file(yaml_name)
+      $log.info "YAML loaded: #{yaml_name}"
     rescue ArgumentError => e
       fail(e.full_message("- Parsing YAML #{yaml_name}; "))
     end
@@ -68,11 +67,16 @@ class FotoEvent
   class FatalError < StandardError; end
 
   # Instance attributes and methods
-  attr_accessor :title_ru, :title_en, :date_start, :date_end
+  attr_reader :directory_name_ru, :directory_name_en
+  attr_reader :title_ru, :title_en, :date_start, :date_end, :author_nikname, :author
+  attr_reader :character_set, :keywords
+  attr_reader :gps_latitude, :gps_latitude_ref, :gps_longitude, :gps_longitude_ref, :gps_altitude, :gps_altitude_ref
 
   def initialize options
+    @title_ru = options[:event][:title_ru].strip
+    @title_en = options[:event][:title_en].strip
+    
     # Event dates
-    $log.info "Initializing event dates"
     begin 
       @date_start = DateTime.strptime(options[:event][:date_start], $DateTimeFormat)
     rescue Exception => e
@@ -85,9 +89,38 @@ class FotoEvent
     end
     raise FatalError, "date_end must be >= date_begin" unless  @date_end >= @date_start 
     $log.info "Event dates: #{@date_start.to_date}..#{@date_end.to_date}"
+    
+    # Generate directory name. Format: PREFIX title
+    @directory_name_ru = "#{directory_name_prefix(@date_start, @date_end)} #{@title_ru}".strip
+    @directory_name_en = "#{directory_name_prefix(@date_start, @date_end)} #{@title_en}".strip
+    
+    @author_nikname = options[:event][:author_nikname]
+    @author = options[:event][:author]
+    
+    @character_set = options[:event][:character_set]||"UTF8"
+    @keywords = options[:event][:keywords]
+    
+    @gps_latitude = options[:event][:gps_latitude]
+    @gps_latitude_ref = options[:event][:gps_latitude_ref]
+    @gps_longitude = options[:event][:gps_longitude]
+    @gps_longitude_ref = options[:event][:gps_longitude_ref]
+    @gps_altitude = options[:event][:gps_altitude]
+    @gps_altitude_ref = options[:event][:gps_altitude_ref]
   end
-
-end
+  
+  private
+  # Generate PREFIX in format:
+  #     YYYYmmdd if date1 == date2
+  #     YYYYmmdd-dd if day is different
+  #     YYYYmmdd-mmdd if month is different
+  #     YYYYmmdd-YYYYmmdd if year is different
+  def directory_name_prefix(date1, date2)
+    prefix = date1.strftime('%Y%m%d')
+    return prefix += "-"+date2.strftime('%Y%m%d') if date1.year != date2.year
+    return prefix += "-"+date2.strftime('%m%d') if date1.mon != date2.mon
+    return prefix += "-"+date2.strftime('%d') if date1.mday != date2.mday
+  end
+end # class
 
 # *** Foto object ***
 class FotoObject
@@ -173,21 +206,21 @@ begin #*** GLOBAL BLOCK
   $log = Logger.new(File.basename((__FILE__), File.extname(__FILE__))+".log")
   $log.level = Logger::DEBUG #DEBUG < INFO < WARN < ERROR < FATAL < UNKNOWN
   $log.info "*** STARTING command #{__FILE__} #{ARGV.inspect}"
-  $log.info "Current dir is #{Dir.pwd}"
+  $log.info "Current dir: #{Dir.pwd}"
 
   dir_to_process, options = read_input_params
 
   foto_event = FotoEvent.new options
+p foto_event  
 
   FotoObject.init_collection(dir_to_process, options[:input_parameter][:foto_ext], foto_event)
 
   FotoObject.collection.each do |foto|
-      print foto.name; puts foto.extention
-      p foto unless foto.errors.empty?
-      #puts "DTO = #{foto.date_time_original}"
-      #puts "FMD = #{foto.file_modify_date}"
-
-    end
+    print foto.name; puts foto.extention
+    #p foto unless foto.errors.empty?
+    #puts "DTO = #{foto.date_time_original}"
+    #puts "FMD = #{foto.file_modify_date}"
+  end
 
 rescue FotoEvent::FatalError => e
   $log.fatal e.full_backtrace_message 
